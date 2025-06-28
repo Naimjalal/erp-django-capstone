f1fce1b81ac7e8a4ec9d69d64f30a2e5e08076ca
naimjalal <naimjalal@gmail.com>
2025-06-28 18:55:05 +0300

    created inventory form

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('categories/add/', views.add_inventory_category, name='add_inventory_category'),
     path('items/add/', views.add_inventory_item, name='add_inventory_item'),
+    path('items/edit/<int:item_id>/', views.edit_inventory_item, name='edit_inventory_item'),
+    path('items/delete/<int:item_id>/', views.delete_inventory_item, name='delete_inventory_item'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .forms import InventoryCategoryForm, InventoryItemForm
+from .models import Inventory_Item
 # Create your views here.
 
 def add_inventory_category(request):
@@ -22,5 +23,28 @@ def add_inventory_item(request):
     else:
         form = InventoryItemForm()
     
-    return render(request, 'inventory/add_inventory_item.html', {'form': form})
-    
+    items = Inventory_Item.objects.select_related('category').all()
+    return render(request, 'inventory/add_inventory_item.html', {'form': form,'items':items
+    })
+
+from django.shortcuts import get_object_or_404
+
+def edit_inventory_item(request, item_id):
+    item = get_object_or_404(Inventory_Item, pk=item_id)
+
+    if request.method == 'POST':
+        form = InventoryItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('add_inventory_item')
+    else:
+        form = InventoryItemForm(instance=item)
+
+    items = Inventory_Item.objects.all()
+    return render(request, 'inventory/add_inventory_item.html', {'form': form, 'items': items})
+
+def delete_inventory_item(request, item_id):
+    item = get_object_or_404(Inventory_Item, pk=item_id)
+    item.delete()
+    return redirect('add_inventory_item')
+ 
